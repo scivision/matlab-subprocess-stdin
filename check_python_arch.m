91e0b77c7b7431877aa41d920333bb7a6231dfce
Michael Hirsch <scivision@users.noreply.github.com>
2022-06-15 09:57:18 -0400

    check_python: also check Windows

--- check_python_arch.m
+++ check_python_arch.m
@@ -3,26 +3,42 @@ function ok = check_python_arch()
 % This can be an issues on Apple Silicon with Matlab x86 on Rosetta
 % and native arm64 Python for example
 
-ok = true;
-
-if ~isunix
-  % for Unix-like systems only at this time. As Windows ARM gets more
-  % popular may investigate
-  return
-end
-
-[ret, matlabArch] = system('uname -m');
-assert(ret==0, "uname failed %s", matlabArch)
+ok = false;
 
 p = pyenv;
 pyexe = p.Executable;
 if isempty(pyexe)
-  error("Python is not setup with Matlab. See 'help pyenv'")
+  warning("Python is not setup with Matlab. See 'help pyenv'")
+  return
+end
+
+if isunix
+  [ret, matlabArch] = system('uname -m');
+else
+  [ret, matlabArch] = system('wmic computersystem get systemtype /value');
+end
+if ret~=0
+  warning("getting system arch failed %s", matlabArch)
+  return
 end
 
 [ret, pythonArch] = system(pyexe + ' -c "import platform; print(platform.machine())"');
-assert(ret==0, "failed to run Python %s", pythonArch)
+if ret~=0
+  warning("failed to run Python %s", pythonArch)
+  return
+end
 
-ok = isequal(pythonArch, matlabArch);
+if isunix
+  ok = isequal(pythonArch, matlabArch);
+else
+  if startsWith(pythonArch, "AMD64")
+    ok = contains(matlabArch, "x64-based");
+  elseif startsWith(pythonArch, "arm64")
+    ok = contains(matlabArch, "ARM64-based");
+  else
+    warning("Unknown Python arch %s", pythonArch)
+  end
+
+end
 
 end

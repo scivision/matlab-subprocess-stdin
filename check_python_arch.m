639fa1296088c2e5435c565efd1fc0e53a4daad5
scivision <scivision@users.noreply.github.com>
2023-08-31 00:53:40 -0400

    check python arch: simplify

--- check_python_arch.m
+++ check_python_arch.m
@@ -5,40 +5,18 @@ function ok = check_python_arch()
 
 ok = false;
 
-p = pyenv;
-pyexe = p.Executable;
-if isempty(pyexe)
-  warning("Python is not setup with Matlab. See 'help pyenv'")
-  return
-end
+matlabArch = string(java.lang.System.getProperty("os.arch"));
 
-if isunix
-  [ret, matlabArch] = system('uname -m');
-else
-  [ret, matlabArch] = system('wmic computersystem get systemtype /value');
-end
-if ret~=0
-  warning("getting system arch failed %s", matlabArch)
-  return
-end
+pythonArch = string(py.platform.machine());
 
-[ret, pythonArch] = system(pyexe + ' -c "import platform; print(platform.machine())"');
-if ret~=0
-  warning("failed to run Python %s", pythonArch)
-  return
-end
-
-if isunix
-  ok = isequal(pythonArch, matlabArch);
-else
-  if startsWith(pythonArch, "AMD64")
-    ok = contains(matlabArch, "x64-based");
-  elseif startsWith(pythonArch, "arm64")
-    ok = contains(matlabArch, "ARM64-based");
-  else
-    warning("Unknown Python arch %s", pythonArch)
+ok = isequal(pythonArch, matlabArch);
+if ~ok
+  switch pythonArch
+    case "AMD64", ok = contains(matlabArch, "amd64");
+    case "arm64", ok = contains(matlabArch, "aarch64");
+    case "aarch64", ok = contains(matlabArch, "arm64");
+    otherwise, warning("Unknown Python arch %s", pythonArch)
   end
-
 end
 
 end

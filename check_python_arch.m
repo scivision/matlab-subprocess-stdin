f4df401b19d439581cf14d15fdbfaaf6f9f6c5b3
scivision <scivision@users.noreply.github.com>
2023-09-01 11:48:55 -0400

    check arch: simplify

--- check_python_arch.m
+++ check_python_arch.m
@@ -3,16 +3,13 @@ function ok = check_python_arch()
 % This can be an issues on Apple Silicon with Matlab x86 on Rosetta
 % and native arm64 Python for example
 
-ok = false;
+matlabArch = lower(string(java.lang.System.getProperty("os.arch")));
 
-matlabArch = string(java.lang.System.getProperty("os.arch"));
-
-pythonArch = string(py.platform.machine());
+pythonArch = lower(string(py.platform.machine()));
 
 ok = isequal(pythonArch, matlabArch);
 if ~ok
   switch pythonArch
-    case "AMD64", ok = contains(matlabArch, "amd64");
     case "arm64", ok = contains(matlabArch, "aarch64");
     case "aarch64", ok = contains(matlabArch, "arm64");
     otherwise, warning("Unknown Python arch %s", pythonArch)
